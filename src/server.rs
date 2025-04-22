21331dcfcbe35f1a982f46a2e4e91ba271aac71c
403F <4o3f@proton.me>
2025-04-22 13:37:30 +0800

    feat: implement default error handling for server

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,38 @@
-use actix_web::{App, HttpServer};
-use anyhow::Ok;
+use actix_web::{
+    App, HttpResponse, HttpServer,
+    body::{BoxBody, MessageBody},
+    dev::ServiceResponse,
+    http::header,
+    middleware::{ErrorHandlerResponse, ErrorHandlers},
+};
 use diesel::{
     dsl::{exists, insert_into, select, update},
     prelude::*,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
 use tracing_unwrap::OptionExt;
 
 mod database;
 mod schema;
 mod services;
+
+fn add_error_header<B>(
+    res: ServiceResponse<B>,
+) -> actix_web::Result<ErrorHandlerResponse<BoxBody>> {
+    let status = res.status();
+    let error_msg = status.canonical_reason().unwrap_or("Unknown error");
+
+    let error_json = json!({ "msg": error_msg }).to_string();
+
+    let new_res = HttpResponse::build(status)
+        .insert_header((header::CONTENT_TYPE, "application/json"))
+        .body(error_json);
+
+    Ok(ErrorHandlerResponse::Response(
+        res.into_response(new_res.map_into_right_body()),
+    ))
+}
 
 #[actix_web::main]
 pub async fn serve() -> std::io::Result<()> {
@@ -17,10 +40,15 @@ pub async fn serve() -> std::io::Result<()> {
 
     database::init_database().map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
 
-    HttpServer::new(|| App::new().service(services::get_ip))
-        .bind(("0.0.0.0", server_config.server.port))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(ErrorHandlers::new().default_handler(add_error_header))
+            .service(services::get_ip)
+            .service(services::bind)
+    })
+    .bind(("0.0.0.0", server_config.server.port))?
+    .run()
+    .await
 }
 
 #[derive(Deserialize, Debug)]
--- src/server/services.rs
+++ src/server/services.rs
@@ -1,8 +1,9 @@
-use actix_web::{HttpRequest, HttpResponse, Responder, get};
-use serde::Serialize;
+use actix_web::{HttpRequest, HttpResponse, Responder, get, post, web::Json};
+use serde::{Deserialize, Serialize};
+use tracing_unwrap::OptionExt;
 
 #[get("/ip")]
-pub(super) async fn get_ip(req: HttpRequest) -> impl Responder {
+pub async fn get_ip(req: HttpRequest) -> impl Responder {
     if let Some(value) = req.peer_addr() {
         let ip = value.ip().to_string();
         tracing::info!("Address {:?} requested for IP", ip);
@@ -15,4 +16,16 @@ pub(super) async fn get_ip(req: HttpRequest) -> impl Responder {
         tracing::error!("No IP can be extracted, this SHOULD NOT HAPPEN");
         unreachable!()
     }
+}
+
+#[derive(Deserialize)]
+struct BindRequestBody {
+    mac: String,
+    id: String,
+}
+#[post("/bind")]
+pub async fn bind(body: Json<BindRequestBody>) -> impl Responder {
+    tracing::info!("MAC {} ID {}", body.mac, body.id);
+    
+    HttpResponse::Ok()
 }
