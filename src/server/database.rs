cdc49efabf5f3c9e174e59afb05b322b6717182c
403F <4o3f@proton.me>
2025-04-27 14:47:12 +0800

    fix: try to fix init conn pool

--- src/server/database.rs
+++ src/server/database.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use diesel::{
     connection::SimpleConnection,
     prelude::*,
@@ -14,26 +12,20 @@ pub static DB_CONNECTION_POOL: OnceCell<Pool<ConnectionManager<SqliteConnection>
     OnceCell::new();
 
 #[derive(Debug)]
-pub struct ConnectionOptions {
-    pub enable_wal: bool,
-    pub enable_foreign_keys: bool,
-    pub busy_timeout: Option<Duration>,
-}
+pub struct ConnectionOptions;
 
 impl diesel::r2d2::CustomizeConnection<SqliteConnection, diesel::r2d2::Error>
     for ConnectionOptions
 {
     fn on_acquire(&self, conn: &mut SqliteConnection) -> Result<(), diesel::r2d2::Error> {
         (|| {
-            if self.enable_wal {
-                conn.batch_execute("PRAGMA journal_mode = WAL; PRAGMA synchronous = NORMAL;")?;
-            }
-            if self.enable_foreign_keys {
-                conn.batch_execute("PRAGMA foreign_keys = ON;")?;
-            }
-            if let Some(d) = self.busy_timeout {
-                conn.batch_execute(&format!("PRAGMA busy_timeout = {};", d.as_millis()))?;
-            }
+            conn.batch_execute("PRAGMA busy_timeout = 5000;")?;
+            conn.batch_execute(r#"
+PRAGMA journal_mode = WAL;          -- better write-concurrency
+PRAGMA synchronous = NORMAL;        -- fsync only in critical moments
+PRAGMA wal_autocheckpoint = 1000;   -- write WAL changes back every 1000 pages, for an in average 1MB WAL file. May affect readers if number is increased
+PRAGMA foreign_keys = ON;           -- enforce foreign keys
+            "#)?;
             Ok(())
         })()
         .map_err(diesel::r2d2::Error::QueryError)
@@ -53,11 +45,7 @@ fn run_migrations(
 pub fn get_connection_pool() -> Pool<ConnectionManager<SqliteConnection>> {
     let manager = ConnectionManager::<SqliteConnection>::new("database.db");
     Pool::builder()
-        .connection_customizer(Box::new(ConnectionOptions {
-            enable_wal: true,
-            enable_foreign_keys: true,
-            busy_timeout: Some(Duration::from_secs(30)),
-        }))
+        .connection_customizer(Box::new(ConnectionOptions))
         .test_on_check_out(true)
         .build(manager)
         .expect("Could not build connection pool")
